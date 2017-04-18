cee439990e2dbf1d8284847daf7fe411d5f50006
vvpvvp <alias8516@sina.com.cn>
2017-04-18 19:30:08 +0800

    分页范围

--- heyui/utils/config.js
+++ heyui/utils/config.js
@@ -8,7 +8,7 @@ const config = {
   page: {
     small: false,
     size: 10,
-    sizes: [10, 50, 100, 200],
+    sizes: [10, 20, 50, 100],
     layout: {
       default: 'total,pager,jumper,sizes'
     }
