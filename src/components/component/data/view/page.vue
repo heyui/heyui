e2909f593c4646d6b95c5b881ba14499c2ab399a
vvpvvp <alias8516@sina.com.cn>
2017-04-18 19:12:16 +0800

    优化分页组件，添加排列的参数

--- src/components/component/data/view/page.vue
+++ src/components/component/data/view/page.vue
@@ -2,13 +2,16 @@
   <div class="doc">
     <h2>Pagination 分页</h2>
     <h3>普通分页</h3>
-    <div><Pagination :cur = "current1" :total="total" @change="currentChange"></Pagination></div>
+    <div><Pagination :cur="current1" :total="total" @change="currentChange"></Pagination></div>
     <h3>迷你分页</h3>
-    <div><Pagination :cur = "current2" :total="total" @change="currentChange" :small="true"></Pagination></div>
+    <div><Pagination :cur="current2" :total="total" @change="currentChange" :small="true"></Pagination></div>
     <h3>自定义分页</h3>
-    <div><Pagination :cur = "current3" :total="total" @change="currentChange" layout="total,sizes,pager,jumper"></Pagination></div>
+    <div><Pagination :cur="current3" :total="total" @change="currentChange" layout="total,sizes,pager,jumper"></Pagination></div>
     <h3>自定义分页2</h3>
-    <div><Pagination :cur = "current4" :total="total" @change="currentChange" layout="pager" :small="true" :size="20"></Pagination></div>
+    <div><Pagination :cur="current4" :total="total" @change="currentChange" layout="pager" :small="true" :size="20"></Pagination></div>
+    <h3>排列</h3>
+    <div><Pagination :cur="current5" align="center" :total="total" @change="currentChange"></Pagination></div>
+    <div><Pagination :cur="current6" align="right" :total="total" @change="currentChange"></Pagination></div>
   </div>
 </template>
 
@@ -21,7 +24,9 @@ export default {
       current2: 1,
       current3: 1,
       current4: 1,
-      total: 250,
+      current5: 1,
+      current6: 1,
+      total: 200,
     }
   },
   methods: {
