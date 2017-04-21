2460806f86ad7b870d28e6649dfd2aa3afa3daae
vvpvvp <alias8516@sina.com.cn>
2017-04-21 14:02:17 +0800

    publish至npm暂时commit

--- p.sh
+++ p.sh
@@ -4,13 +4,9 @@ echo "迁移代码"
 cp -R heyui ../zanshi
 echo "切换至gh-pages分支"
 git checkout master
-cp -R ../zanshi/* ./
-# cp index.html 404.html
-# git add -A
-# git commit -m '新版'
-# git pull
-# git push
-
-# echo "切换至doc分支"
-# git checkout doc
-# rm -rf ../gen
+cp -R ../zanshi/heyui/* ./
+npm-version-up
+git add -A
+git commit -m '新版'
+git pull
+git push
