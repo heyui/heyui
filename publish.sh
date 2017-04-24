d6c4650a65d9f8a1675b79550604f7a23bd08606
vvpvvp <alias8516@sina.com.cn>
2017-04-24 10:27:39 +0800

    update shell code

---
+++ publish.sh
@@ -0,0 +1,17 @@
+git add -A
+git commit -m 'publish至npm暂时commit'
+echo "迁移代码"
+cp -R heyui ../zanshi
+echo "切换至master分支"
+git checkout master
+cp -R ../zanshi/* ./
+echo "打包hey build"
+hey b
+echo "更新版本"
+standard-version
+git add -A
+git commit -m 'publish new version'
+git pull
+git push --follow-tags origin master
+echo "发布至npm"
+npm publish
