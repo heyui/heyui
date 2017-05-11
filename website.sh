e43dd6f338e5f203be1f195b96a3673a4713cc13
vvpvvp <alias8516@sina.com.cn>
2017-05-11 11:59:55 +0800

    打包至gh-pages暂时commit

--- website.sh
+++ website.sh
@@ -1,19 +1,24 @@
 git add -A
 git commit -m '打包至gh-pages暂时commit'
 echo "开始打包代码"
+set -e
 hey b
+set -e
 echo "打包代码成功"
 echo "迁移打包后的代码"
 cp -R gen ../
 rm -rf gen
 echo "切换至gh-pages分支"
+set -e
 git checkout gh-pages
+set -e
 git pull
 cp -R ../gen/* ./
 cp index.html 404.html
 git add -A
 git commit -m '新版'
 git push origin gh-pages
+set -e
 
 echo "切换至doc分支"
 git checkout doc
