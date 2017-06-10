c78752381fe8630ca4a00261f2d61ac4bd6a9a38
vvpvvp <alias8516@sina.com.cn>
2017-06-10 14:11:01 +0800

    refactor: 补充steps功能

--- src/components/component/plugin/steps.vue
+++ src/components/component/plugin/steps.vue
@@ -2,6 +2,7 @@
   <div class="doc">
     <h2>Steps 步骤条</h2>
 
+    <h3>基本调用</h3>
     <p>
       <ButtonGroup size="s">
         <Button icon="plus" @click="step>=2||step++"></Button>
@@ -10,6 +11,7 @@
     </p>
     <Steps :datas="data" :step="step"></Steps>
 
+    <h3>自定义Icon, 说明</h3>
     <p>
       <ButtonGroup size="s">
         <Button icon="plus" @click="step1>=3||step1++"></Button>
@@ -18,7 +20,18 @@
     </p>
     <Steps :datas="data1" :step="step1"></Steps>
 
-    <h3>基本调用</h3>
+    <h3>使用key值识别第几步</h3>
+    <p>
+      <ButtonGroup size="s">
+        <Button @click="step2='a'">步骤1</Button>
+        <Button @click="step2='b'">步骤2</Button>
+        <Button @click="step2='c'">步骤3</Button>
+        <Button @click="step2='d'">步骤4</Button>
+      </ButtonGroup>
+    </p>
+
+    <Steps :datas="data" :step="step2"></Steps>
+
 
     <h3>Steps 参数</h3>
     <table class="table">
@@ -30,25 +43,25 @@
         <th>默认值</th>
       </tr>
       <tr>
-        <td>trigger</td>
-        <td>触发方式</td>
-        <td>String</td>
-        <td>click, hover</td>
-        <td>click</td>
-      </tr>
-      <tr>
-        <td>toggleIcon</td>
-        <td>是否显示展示状态的icon</td>
-        <td>Boolean</td>
+        <td>datas</td>
+        <td>选择的数据</td>
+        <td>Array, Object</td>
         <td>-</td>
-        <td>true</td>
+        <td></td>
       </tr>
       <tr>
-        <td>placement</td>
-        <td>menu显示的位置</td>
-        <td>Stirng</td>
-        <td>top, top-start, top-end, bottom, bottom-start, bottom-end, left, left-start, left-end, right, right-start, right-end</td>
-        <td>bottom-start</td>
+        <td>dict</td>
+        <td>调用全局定义的字典</td>
+        <td>String</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>step</td>
+        <td>当前第几步，可以是key</td>
+        <td>Number,String</td>
+        <td>-</td>
+        <td>0</td>
       </tr>
     </table>
   </div>
@@ -63,6 +76,7 @@ export default {
         a: '步骤1',
         b: '步骤2',
         c: '步骤3',
+        d: '步骤4',
       },
       step: 0,
       data1: [
@@ -71,7 +85,8 @@ export default {
         { key: 'c', title: '步骤3', icon: 'h-icon-star-on', desc: '步骤3描述' },
         { key: 'd', title: '步骤4', icon: 'h-icon-complete', desc: '步骤4描述' },
       ],
-      step1: 0
+      step1: 0,
+      step2: 'a'
     }
   },
   methods: {
