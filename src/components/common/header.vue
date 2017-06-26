20016bd124a9d78995f99a39566c3e49a452e2f0
vvpvvp <alias8516@sina.com.cn>
2017-06-26 22:39:28 +0800

    update header

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -8,9 +8,9 @@
         <router-link to="/guide">入门</router-link>
         <router-link to="/component">组件</router-link>
         <router-link to="/resource">资源</router-link>
-        <a href="http://demo.heyui.top/login.html">Demo</a>
+        <a target="_blank" href="http://demo.heyui.top/login.html">demo</a>
         <!-- <router-link to="/about">团队</router-link> -->
-        <a href="https://github.com/heyui/heyui">Github</a>
+        <a target="_blank" href="https://github.com/heyui/heyui"><i class="h-icon-github" v-font='20'></i></a>
       </div>
     </div>
   </header>
