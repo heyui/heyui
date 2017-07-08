90c33d1e20a6ada8ab110a5c6ed3fb4ab5905318
vvpvvp <alias8516@sina.com.cn>
2017-07-08 14:18:05 +0800

    fix: 样式优化第二波

--- doc/components/component/basic/color.vue
+++ doc/components/component/basic/color.vue
@@ -46,22 +46,12 @@
 
     <h3>灰色</h3>
     <Row :space='20' class="color-spans">
-      <Col :width='8'>
+      <Col :width='12'>
       <div v-bg-color:dark  v-color:white>
         <p>@dark-color</p>
       </div>
       </Col>
-      <Col :width='8'>
-      <div v-bg-color:dark1  v-color:white>
-        <p>@dark1-color</p>
-      </div>
-      </Col>
-      <Col :width='8'>
-      <div v-bg-color:dark2  v-color:white>
-        <p>@dark2-color</p>
-      </div>
-      </Col>
-      <Col :width='24'>
+      <Col :width='12'>
       <div v-bg-color:gray  v-color:white>
         <p>@gray-color</p>
       </div>
--- doc/components/guide/themes.vue
+++ doc/components/guide/themes.vue
@@ -7,79 +7,70 @@
     <p>更换的方法已经在快速上手的文档中有详细的说明，如果有任何疑问，请参考<a href="/guide/quickstart">快速上手</a>文档。</p>
 
     <h3>全局变量说明</h3>
-    <p>我们的原始全局变量文件如下，<a href="">去查看</a></p>
+    <p>我们的原始全局变量文件你可以在<a href="https://github.com/heyui/heyui/blob/master/themes/var.less" target="_blank">github</a>上查看，以下将列出你可以覆盖修改参数，完成系统主题修改的目的。</p>
 <codes type="less">
-@prefix : h-;
 // Color
 @primary-color : #3399ff;
-@light-primary-color : lighten(@primary-color, 10%);
-@dark-primary-color : darken(@primary-color, 10%);
 @link-color : @primary-color;
 @blue-color : #3B91FF; //info
 @green-color : #13CE66; //success
 @yellow-color : #FFAE00; //warn
 @red-color : #E11617; //error
 @white-color : #ffffff;
+
 //Gray 1-4 more shallow
+//暗黑的字体颜色
 @dark-color: #333333;
-@dark1-color: #555555;
-@dark2-color: #666666;
+
+//灰色的字体
 @gray-color: #808080;
+//浅灰色的字体，或者深色的线条
 @gray1-color: #d3d3d3;
+//浅灰色的字体，或者深色的线条
 @gray2-color: #eeeeee;
+//一般线条的颜色
 @gray3-color: #f3f3f3;
+//一般灰色的背景
 @gray4-color: #f9f9f9;
-//border
-@border-color : @gray2-color;
-@border: 1px solid @gray2-color;
-@border-darken-color : @gray1-color;
-//disabled
-@disabled-color: @dark1-color;
-//hover
-@hover-background-color: lighten(@primary-color, 37%);
+
 //Base
-@background : #FFF;
 @font-size : 14px;
 @font-size-mini : 13px;
 @line-height : 1.5;
+@line-height-computed : floor((@font-size * @line-height));
+
+@font-family : "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+
+//border
+@border-color: @gray2-color;
+@border: 1px solid @gray2-color;
+
+//disabled
+@disabled-cursor: not-allowed;
+@disabled-color: @gray-color;
+@disabled-border-color: @gray1-color;
+@disabled-background-color: @gray4-color;
+
+//hover
+@hover-background-color: lighten(@primary-color, 37%);
+
+//radiu
 @border-radius : 4px;
 @btn-border-radius: 4px;
-@line-height-computed : floor((@font-size * @line-height));
-@font-family : "Helvetica Neue",
-Helvetica,
-"PingFang SC",
-"Hiragino Sans GB",
-"Microsoft YaHei",
-"微软雅黑",
-Arial,
-sans-serif;
-@shadow-color : rgba(0, 0, 0, .2);
+
+//box-shadow
 @box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
-@shadow-base: 0 1px 6px rgba(0,0,0,.2);
-@disabled-cursor: not-allowed;
-@icon-prefix: h-icon-;
+@box-shadow-notify: 0 5px 10px rgba(0, 0, 0, 0.1);
+@box-shadow-dropdown : 0 5px 10px rgba(0, 0, 0, 0.1);
+@box-shadow-tooltip : 1px 1px 0px rgba(0, 0, 0, .2);
+@box-shadow-select-dropdown: 0 1px 6px rgba(0, 0, 0, .2);
+
 //dropdown,tooltip触发的el添加相关class
 @pop-ref-prefix: h-pop-trigger;
 // Input
 @input-height : 30px;
 // Tag
 @tag-font-size : 12px;
-// Z-index
-@zindex-loading : 8;
-@zindex-affix : 10;
-@zindex-backtop : 10;
-@zindex-select : 1000;
-@zindex-dropdown : 1000;
-@zindex-modal : 1000;
-@zindex-message : 1010;
-@zindex-notify : 1010;
-@zindex-tooltip : 1060;
-@zindex-loading-fullscreen : 2000;
-@zindex-loading-bar : 2000;
-// Animation
-@animation-time : .3s;
-@transition-time : .2s;
-// // Slider
 </codes>
 
   </div>
