de9dba92c9a5a961c792c8da21ca21e0cb8cb3c6
vvpvvp <alias8516@sina.com.cn>
2017-11-01 14:40:20 +0800

    fix: tabs click trigger time

--- src/components/tabs/tabs.vue
+++ src/components/tabs/tabs.vue
@@ -38,12 +38,11 @@ export default {
   },
   methods: {
     trigger(data, index) {
-      this.$emit('click', data, index);
-      if (this.value == data[this.key]) {
-        return;
+      if (this.value != data[this.key]) {
+        this.$emit('input', data[this.key]);
+        this.$emit('change', data, index);
       }
-      this.$emit('input', data[this.key]);
-      this.$emit('change', data, index);
+      this.$emit('click', data, index);
     }
   },
   computed: {
