fe7aa96fa828b7d3b3a0f47a58d82d693f191acf
vvpvvp <alias8516@sina.com.cn>
2017-12-18 11:57:03 +0800

    fix: delete unusefull configs

--- src/locale/lang/zh-CN.js
+++ src/locale/lang/zh-CN.js
@@ -32,54 +32,6 @@ const lang = {
       upload: '上传',
       reUpload: '重新上传'
     },
-    table: {
-      noDataText: '暂无数据',
-      noFilteredDataText: '暂无筛选结果',
-      confirmFilter: '筛选',
-      resetFilter: '重置',
-      clearFilter: '全部'
-    },
-    datepicker: {
-      weeks: ['日', '一', '二', '三', '四', '五', '六'],
-      months: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一', '十二'],
-      selectDate: '选择日期',
-      selectTime: '选择时间',
-      startTime: '开始时间',
-      endTime: '结束时间',
-      clear: '清空',
-      ok: '确定',
-      datePanelLabel: '[yyyy年] [m月]',
-      month: '月',
-      year: '年',
-      weekStartDay: '0',
-    },
-    modal: {
-      okText: '确定',
-      cancelText: '取消'
-    },
-    poptip: {
-      okText: '确定',
-      cancelText: '取消'
-    },
-    page: {
-      prev: '上一页',
-      next: '下一页',
-      total: '共',
-      item: '条',
-      items: '条',
-      prev5: '向前 5 页',
-      next5: '向后 5 页',
-      page: '条/页',
-      goto: '跳至',
-      p: '页'
-    },
-    rate: {
-      star: '星',
-      stars: '星'
-    },
-    tree: {
-      emptyText: '暂无数据'
-    }
   }
 };
 
