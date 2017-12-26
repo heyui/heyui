e41ac42a7f5b01f4d856be8d41384c2323c562b7
vvpvvp <alias8516@sina.com.cn>
2017-12-26 13:15:33 +0800

    fix: component add key

--- doc/components/common/example.vue
+++ doc/components/common/example.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="demo-box">
     <div class="demo" v-if="noDemo!==true">
-      <component :is="com"></component>
+      <component :is="com" :key="com"></component>
     </div>
     <pre v-highlightjs="sourcecode" :class="{'expand':expand}"><code class="html"></code></pre>
     <div class="expand-button link" @click="expand=!expand">{{expand?'收起':'展开'}}代码</div>
