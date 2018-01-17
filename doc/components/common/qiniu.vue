331310ea7095ea52c1e9512dc7fdfe1a4b933be9
vvpvvp <alias8516@sina.com.cn>
2018-01-17 10:16:00 +0800

    fix: demo->admin

--- doc/components/common/qiniu.vue
+++ doc/components/common/qiniu.vue
@@ -50,7 +50,7 @@ export default {
       let param = {
         runtimes: 'html5',
         browse_button: this.$refs.uploader.getBrowseButton(),
-        uptoken_url: 'http://demo.heyui.top/api/uptoken',
+        uptoken_url: 'http://admin.heyui.top/api/uptoken',
         domain: 'http://oroc6hc3j.bkt.clouddn.com',
         chunk_size: '4mb',
         unique_names: true,
