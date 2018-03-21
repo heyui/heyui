829fb194c2ff78b9cab46997ee04930d10d9aebe
vvpvvp <alias8516@sina.com.cn>
2018-03-21 20:32:00 +0800

    fix: dictMapping 添加connector参数

--- src/filters/index.js
+++ src/filters/index.js
@@ -2,7 +2,7 @@ import utils from '../utils/utils'
 import config from '../utils/config'
 
 export default {
-  dictMapping(value, key, connector) {
+  dictMapping(value, key, connector = ', ') {
     let dict = config.getDict(key);
     if (!dict || utils.isNull(value)) return '';
     if (connector) {
@@ -32,6 +32,6 @@ export default {
         return d[titleField];
       }
       return d;
-    }).filter(ele => (ele && ele !== '')).join(', ');
+    }).filter(ele => (ele && ele !== '')).join(connector);
   }
 }
