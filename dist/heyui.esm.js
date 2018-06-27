72688a995fc03292372faa4e9bd7b844490f1de3
vvpvvp <alias8516@sina.com.cn>
2018-06-27 13:47:38 +0800

    build dist

--- dist/heyui.esm.js
+++ dist/heyui.esm.js
@@ -1,7 +1,7 @@
 /**
   * heyui - UI Toolkit for Web, Vue2.0.
   *
-  * @version v1.3.0-alpha.2
+  * @version v1.3.0-alpha.3
   * @homepage http://www.heyui.top
   * @copyright Copyright (C) Lan 2017-2018
   * @license MIT
