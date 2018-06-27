a6db775e55f3fbf18970bab7c3ce3c04f086bc77
vvpvvp <alias8516@sina.com.cn>
2018-06-27 15:12:04 +0800

    fix(circle): circle use path svg for show

--- src/components/circle/circle.vue
+++ src/components/circle/circle.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="h-circle" :style="circleStyle">
-    <svg :width="circleSize" :height="circleSize" :viewBox="`0 0 ${circleSize*2} ${circleSize*2}`">
+    <!-- <svg :width="circleSize" :height="circleSize" :viewBox="`0 0 ${circleSize*2} ${circleSize*2}`">
       <circle :cx="circleSize" :cy="circleSize" :r="size" :stroke-width="strokeWidth" stroke="#f3f3f3" fill="none"></circle>
       <circle :cx="circleSize" :cy="circleSize" :r="size" stroke-linecap="round" :stroke-width="strokeWidth" fill="none" :transform="`matrix(0,-1,1,0,0,${circleSize*2})`" :stroke="color" :stroke-dasharray="countPercent"></circle>
+    </svg> -->
+    <svg :width="circleSize" :height="circleSize" :viewBox="`0 0 ${circleSize*2} ${circleSize*2}`">
+      <path :d="pathString" stroke="#f3f3f3" :stroke-width="strokeWidth" fill-opacity="0"></path>
+      <path :d="pathString" stroke-linecap="round" :stroke="color" :stroke-width="strokeWidth" fill-opacity="0" :style="pathStyle"></path>
     </svg>
     <div class="h-circle-content">
       <slot></slot>
@@ -47,32 +51,45 @@ export default {
     circleSize() {
       return this.size + (this.strokeWidth / 2);
     },
-    countPercent() {
-      let percent = this.percent / 100;
-      let perimeter = Math.PI * 2 * this.size;
-      return `${perimeter * percent} ${perimeter * (1 - percent)}`;
+    radius () {
+      return this.circleSize - this.strokeWidth / 2;
     },
-    circleInnerStyle() {
-      let s = {};
-      s.height = `${this.strokeWidth}px`;
-      return s;
+    pathString () {
+        return `M ${this.circleSize},${this.circleSize} m 0,-${this.radius}
+        a ${this.radius},${this.radius} 0 1 1 0,${2 * this.radius}
+        a ${this.radius},${this.radius} 0 1 1 0,-${2 * this.radius}`;
     },
-    circleBgStyle() {
-      let s = {};
-      if (!colors.includes(this.color)) {
-        s['background-color'] = this.color;
-      }
-      s.width = `${this.percent}%`;
-      s.height = `${this.strokeWidth}px`;
-      return s;
+    len () {
+        return Math.PI * 2 * this.radius;
     },
-    circleBgClass() {
-      let s = {};
-      if (colors.includes(this.color)) {
-        s[`bg-${this.color}-color`] = true
-      }
-      return s;
-    }
+    pathStyle () {
+      return {
+        'stroke-dasharray': `${this.len}px ${this.len}px`,
+        'stroke-dashoffset': `${((100 - this.percent) / 100 * this.len)}px`,
+        'transition': 'stroke-dashoffset 0.6s ease 0s, stroke 0.6s ease'
+      };
+    },
+    // circleInnerStyle() {
+    //   let s = {};
+    //   s.height = `${this.strokeWidth}px`;
+    //   return s;
+    // },
+    // circleBgStyle() {
+    //   let s = {};
+    //   if (!colors.includes(this.color)) {
+    //     s['background-color'] = this.color;
+    //   }
+    //   s.width = `${this.percent}%`;
+    //   s.height = `${this.strokeWidth}px`;
+    //   return s;
+    // },
+    // circleBgClass() {
+    //   let s = {};
+    //   if (colors.includes(this.color)) {
+    //     s[`bg-${this.color}-color`] = true
+    //   }
+    //   return s;
+    // }
   }
 };
 </script>
