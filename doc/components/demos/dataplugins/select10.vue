34f6c4fe42e55c303a07d382621ada3e3dba3605
vvpvvp <alias8516@sina.com.cn>
2018-07-04 17:36:15 +0800

    docs: fix select10 param

--- doc/components/demos/dataplugins/select10.vue
+++ doc/components/demos/dataplugins/select10.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <p><h-switch v-model="disabled" :small="true">禁用</h-switch></p>
-    <p v-width="300"><Select v-model="select" :datas="param" :readonly = "disabled" placeholder="请选择您的内容"></Select></p>
-    <p v-width="300"><Select v-model="select" filterable :datas="param" :readonly = "disabled" placeholder="请选择您的内容"></Select></p>
-    <p v-width="300"><Select v-model="select1" multiple :datas="param" :readonly = "disabled" placeholder="请选择您的内容"></Select></p>
-    <p v-width="300"><Select v-model="select1" multiple filterable :datas="param" :readonly = "disabled" placeholder="请选择您的内容"></Select></p>
+    <p v-width="300"><Select v-model="select" :datas="param" :disabled = "disabled" placeholder="请选择您的内容"></Select></p>
+    <p v-width="300"><Select v-model="select" filterable :datas="param" :disabled = "disabled" placeholder="请选择您的内容"></Select></p>
+    <p v-width="300"><Select v-model="select1" multiple :datas="param" :disabled = "disabled" placeholder="请选择您的内容"></Select></p>
+    <p v-width="300"><Select v-model="select1" multiple filterable :datas="param" :disabled = "disabled" placeholder="请选择您的内容"></Select></p>
   </div>
 </template>
 <script>
