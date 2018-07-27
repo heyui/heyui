b12506726289b5fe068b079ba3978128f9038ea0
vvpvvp <alias8516@sina.com.cn>
2018-07-27 13:56:50 +0800

    docs: add docs for scrollintoview

--- doc/components/component/other/scrollintoview.vue
+++ doc/components/component/other/scrollintoview.vue
@@ -4,6 +4,20 @@
 
     <h3>基本调用</h3>
     <example demo="other/scrollintoview1"></example>
+
+    <codes type="javascript">scrollIntoView(someElement, {
+  time: 500, // 半秒
+  align:{
+    top: 0 to 1, // default 0.5 (center)
+    left: 0 to 1, // default 0.5 (center)
+    topOffset: 0, // 偏移顶部对齐的像素
+    leftOffset: 0, // 偏移左边对齐的像素
+  },
+  isScrollable: function(target, defaultIsScrollable){
+    // 判断对象是否可以滚动，针对有多层scroll父层的情况
+    return defaultIsScrollable(target) || ~target.className.indexOf('scrollable');
+  }
+});</codes>
     
 
     <h3>ScrollIntoView 参数</h3>
@@ -25,6 +39,13 @@
       <tr>
         <td>align</td>
         <td>位置，包含top, topOffset</td>
+        <td></td>
+        <td>-</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>isScrollable</td>
+        <td>判断对象是否可以滚动，针对有多层scroll父层的情况</td>
         <td></td>
         <td>-</td>
         <td></td>
--- doc/components_en/component/other/scrollintoview.vue
+++ doc/components_en/component/other/scrollintoview.vue
@@ -5,6 +5,19 @@
     <h3>Basic</h3>
     <example demo="other/scrollintoview1"></example>
     
+    <codes type="javascript">scrollIntoView(someElement, {
+  time: 500, // half a second
+  align:{
+    top: 0 to 1, // default 0.5 (center)
+    left: 0 to 1, // default 0.5 (center)
+    topOffset: 0, // pixels to offset top alignment
+    leftOffset: 0, // pixels to offset left alignment
+  },
+  isScrollable: function(target, defaultIsScrollable){
+    // Determines whether objects can be scrolled against a multi-layer scroll parent layer.
+    return defaultIsScrollable(target) || ~target.className.indexOf('scrollable');
+  }
+});</codes>
 
     <h3>ScrollIntoView 参数</h3>
     <table class="table">
@@ -17,14 +30,21 @@
       </tr>
       <tr>
         <td>time</td>
-        <td>位移消耗的时间</td>
+        <td>Time of displacement consumption</td>
         <td>Number</td>
         <td>-</td>
         <td>0</td>
       </tr>
       <tr>
         <td>align</td>
-        <td>位置，包含top, topOffset</td>
+        <td>Location, including top, topOffset</td>
+        <td></td>
+        <td>-</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>isScrollable</td>
+        <td>Determines whether objects can be scrolled against a multi-layer scroll parent layer.</td>
         <td></td>
         <td>-</td>
         <td></td>
