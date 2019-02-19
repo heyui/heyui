036b180ecb6f984f461e945b85ede95a24d90311
Emrah ATILKAN <emrah.atilkan@proj-e.com>
2019-02-19 18:42:31 +0300

    minor translations

--- doc/components_en/about.vue
+++ doc/components_en/about.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="about">
     <div class="page-body text-center">
-      我们的团队
+      Hello World
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        pass: '',
-        error: false
-      }
-    },
-    methods: {},
-    components: {}
-  }
+export default {
+	data() {
+		return {
+			pass: "",
+			error: false
+		};
+	},
+	methods: {},
+	components: {}
+};
 </script>
