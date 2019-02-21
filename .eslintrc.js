39a2096f77726f75ba6e5187b377a9815d2c255b
vvpvvp <alias8516@sina.com.cn>
2019-02-21 10:03:36 +0800

    chore: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
   "root": true,
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true
+    },
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
   "env": {
     "browser": true,
     "commonjs": true,
     "es6": true
   },
-  "globals": {
-  },
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "ecmaFeatures": {
-      "jsx": true
-    }
-  },
-  "extends": 'airbnb-base',
+  "extends": "eslint:recommended",
+  "plugins": ["vue"],
   "rules": {
+    "no-console": ["error", { allow: ["warn", "error"] }]
   }
 }
