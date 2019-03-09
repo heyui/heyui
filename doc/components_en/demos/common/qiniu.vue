c891be60deed2b41b572730daa92630d4a15fa26
vvpvvp <alias8516@sina.com.cn>
2019-03-09 23:36:03 +0800

    docs: update qiniu link

--- doc/components_en/demos/common/qiniu.vue
+++ doc/components_en/demos/common/qiniu.vue
@@ -56,7 +56,7 @@ export default {
         runtimes: 'html5',
         browse_button: this.$refs.uploader.getBrowseButton(),
         uptoken_url: 'https://www.heyui.top/api/uptoken',
-        domain: '//oroc6hc3j.bkt.clouddn.com',
+        domain: '//img.heyui.top',
         chunk_size: '4mb',
         unique_names: true,
         auto_start: false,
