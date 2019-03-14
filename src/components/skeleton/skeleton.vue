cf3ae5e36c7059fd3ec8c7b53e5d943438bd8913
Aaron <kongyajie0704@gmail.com>
2019-03-14 21:40:42 +0800

    feat:添加Skeleton加载占位图

--- src/components/skeleton/skeleton.vue
+++ src/components/skeleton/skeleton.vue
@@ -2,12 +2,46 @@
 </style>
 <template>
   <div class="h-skeleton">
-    <p></p>
+    <div class="h-skeleton-content" v-if="loading" :class="{'h-skeleton-active' : active}">
+      <h3 class="h-skeleton-title" :style="{'width': titleWidth}"></h3>
+      <ul class="h-skeleton-paragraph">
+        <li v-for="(item, index) in rows" :key="index"></li>
+      </ul>
+    </div>
+    <template v-else>
+      <slot></slot>
+    </template>
   </div>
 </template>
 <script>
 export default {
   name: 'hSkeleton',
+  props: {
+    loading: {
+      type: Boolean,
+      default: true
+    },
+    active: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: Boolean,
+      default: true,
+    },
+    paragraph: {
+      type: Boolean,
+      default: true,
+    },
+    titleWidth: {
+      type: String,
+      default: '38%'
+    },
+    rows: {
+      type: Number,
+      default: 3
+    },
+  },
   data() {
     return {
 
