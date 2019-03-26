a17eab32a7e860f6e5f571b9f125b36ea859a567
vvpvvp <alias8516@sina.com.cn>
2019-03-26 10:30:13 +0800

    chore: update create.js

---
+++ src/components/cascader/cascader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="h-cascader">
+  </div>
+</template>
+<script>
+export default {
+  name: 'hCascader',
+  props: {
+  },
+  data() {
+    return {
+
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+
+    }
+  },
+  computed: {
+
+  }
+};
+</script>
---
+++ src/components/cascader/index.js
@@ -0,0 +1,3 @@
+import Cascader from './cascader';
+
+export default Cascader;
