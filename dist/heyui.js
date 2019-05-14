d438b5a4e4e99d43c228a57893332a9c89a3f6cd
vvpvvp <alias8516@sina.com.cn>
2019-05-14 16:54:43 +0800

    build dist

--- dist/heyui.js
+++ dist/heyui.js
@@ -1,7 +1,7 @@
 /**
   * heyui - UI Toolkit for Web, Vue2.0.
   *
-  * @version v1.20.0-alpha.2
+  * @version v1.20.0-alpha.3
   * @homepage http://www.heyui.top
   * @copyright Copyright © 2017-present Lan
   * @license MIT
