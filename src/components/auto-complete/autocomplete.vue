e744898753864e119256e4ee750ca0cce39c46ab
vvpvvp <alias8516@sina.com.cn>
2019-07-02 10:35:53 +0800

    fix(AutoComplete): add  autocomplete="off" on input

--- src/components/auto-complete/autocomplete.vue
+++ src/components/auto-complete/autocomplete.vue
@@ -2,11 +2,11 @@
   <div :class="autocompleteCls">
     <div :class="showCls">
       <template v-if="multiple"><span v-for="(obj, index) of objects" :key="index+''+obj.key"><span>{{obj.title}}</span><i class="h-icon-close-min" @click.stop="remove(obj)" v-if="!disabled"></i></span>
-        <input :disabled="disabled" ref="input" type="text" class="h-autocomplete-input h-input" @focus="focus" v-model="tempValue" @blur.stop="blur" @paste="paste" @keyup="handle" @keydown="keydownHandle" @keypress.enter="enterHandle" :placeholder="showPlaceholder" />
+        <input :disabled="disabled" ref="input" type="text" class="h-autocomplete-input h-input" @focus="focus" v-model="tempValue" @blur.stop="blur" @paste="paste" @keyup="handle" @keydown="keydownHandle" @keypress.enter="enterHandle" :placeholder="showPlaceholder" autocomplete="off" />
         <i class="h-icon-loading" v-if="loading"></i>
       </template>
       <template v-if="!multiple">
-        <input type="text" ref="input" :disabled="disabled" class="h-autocomplete-input h-input" @focus="focus" v-model="tempValue" @paste="paste" @blur.stop="blur" @keyup="handle" @keypress.enter="enterHandle" :placeholder="showPlaceholder" />
+        <input type="text" ref="input" :disabled="disabled" class="h-autocomplete-input h-input" @focus="focus" v-model="tempValue" @paste="paste" @blur.stop="blur" @keyup="handle" @keypress.enter="enterHandle" :placeholder="showPlaceholder" autocomplete="off" />
         <i class="h-icon-loading" v-if="loading"></i>
         <i class="h-icon-close text-hover" v-else-if="tempValue&&!disabled" @mousedown="clear"></i>
       </template>
