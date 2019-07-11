89e670e4c83a939a9f213bc9b7ddacf3c82f780e
vvpvvp <alias8516@sina.com.cn>
2019-07-11 15:50:14 +0800

    docs: 文案修正

--- doc/components/run/components/run-error.vue
+++ doc/components/run/components/run-error.vue
@@ -9,52 +9,30 @@
     <template v-else>
     <h3>Run系统使用注意事项：</h3>
     <p>Notes for Run System Use:</p>
-    <p>编写的代码，请严格按照style, template, script模式编写。</p>
+    <p>1、编写的代码，请严格按照style, template, script模式编写。</p>
     <p>Write the code in strict accordance with style, template, script mode.</p>
-    <p>在script标签中，不允许使用其他的代码方式，只允许使用以下方式定义： </p>
-    <p>In script tags, no other code is allowed, only the following ways are allowed to define:</p>
-    <codes type="javascript">export default {
-    data: function () {
-      return {
-        loading1:false,
-        loading2:false
-      }
-    }
-  }</codes>
 
-    <p>请注意，以下情况会导致代码编译错误：</p>
+<codes type="html" :content="sample"></codes>
+    <p>2、less 中可以使用全局less变量</p>
+    <p>具体的参数请参考：<a href="https://www.heyui.top/component/themes#%E5%85%A8%E5%B1%80%E5%8F%98%E9%87%8F%E8%AF%B4%E6%98%8E">主题全局变量</a></p>
+
+    <p class="red-color">请注意，以下情况会导致代码编译错误：</p>
     <p>Note that the following can lead to code compilation errors:</p>
 
     <p>使用import / Use import</p>
     <codes type="javascript">import jsonp from 'fetch-jsonp';</codes>
-
-    <p>定义外部Function / Define Function outside</p>
-
-    <codes type="javascript">const loadData = function (filter, callback) {
-jsonp(`https://suggest.taobao.com/sug?code=utf-8&q=${filter}`)
-  .then(response => response.json())
-  .then(d => {
-    callback(
-      d.result.map(r => {
-        return {
-          name: r[0],
-          code: r[1] + Math.random()
-        };
-      })
-    );
-  });
-};</codes>
 </template>
   </div>
 </template>
 <script>
+import sample from '../js/example';
 export default {
   props: {
     err: String
   },
   data() {
     return {
-
+      sample
     };
   },
   mounted() {
