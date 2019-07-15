7a759612d5b0277053fb5717f8225b572d47edf6
vvpvvp <alias8516@sina.com.cn>
2019-07-15 21:48:55 +0800

    fix(Switch): add slot open and close
    Closes #184

--- doc/components/demos/form/switch1.vue
+++ doc/components/demos/form/switch1.vue
@@ -4,6 +4,19 @@
     <div><h-switch v-model="check1" :trueValue="1" :falseValue="2">1/2</h-switch></div>
     <div>{{check2}}</div>
     <div><h-switch v-model="check2" trueValue="yes" falseValue="no" :small="true">yes/no</h-switch></div>
+    <div>{{check3}}</div>
+    <div>
+      <h-switch v-model="check3">
+        <span slot="open">开</span>
+        <span slot="close">关</span>
+      </h-switch>
+    </div>
+    <div>{{check4}}</div>
+    <div>
+      <h-switch v-model="check4">
+        <span slot="open" class="h-icon-check"></span>
+        <span slot="close" class="h-icon-close"></span>
+      </h-switch></div>
   </div>
 </template>
 
@@ -13,7 +26,9 @@ export default {
   data() {
     return {
       check1: 2,
-      check2: 'no'
+      check2: 'no',
+      check3: false,
+      check4: false
     };
   }
 };
