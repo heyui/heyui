0c5fce86fb6772c23d7091be68d607fa44e7a50b
vvpvvp <alias8516@sina.com.cn>
2019-07-17 11:29:34 +0800

    chore(release): 1.23.3

--- dist/heyui.esm.js
+++ dist/heyui.esm.js
@@ -1,7 +1,7 @@
 /**
   * heyui - UI Toolkit for Web, Vue2.0.
   *
-  * @version v1.23.3-alpha.0
+  * @version v1.23.3
   * @homepage http://www.heyui.top
   * @copyright Copyright © 2017-present Lan
   * @license MIT
