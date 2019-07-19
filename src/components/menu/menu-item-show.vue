d96273f3f865ec1263543659301bcf0f7b61c126
zxlau <657413201@qq.com>
2019-07-19 11:04:53 +0800

    feat: update menu

--- src/components/menu/menu-item-show.vue
+++ src/components/menu/menu-item-show.vue
@@ -5,7 +5,7 @@
       <span class='h-menu-show-icon' v-show='data.icon'><i :class="data.icon"></i></span>
       <span class='h-menu-show-desc'>{{data.title}}</span>
       <span class='h-menu-show-count' v-if="data.count"><Badge :count="data.count" :max-count="99"></Badge></span>
-      <span class='h-menu-show-expand' v-if="data.children&&data.children.length>0">
+      <span class='h-menu-show-expand' v-if="mode!='horizontal'&&data.children&&data.children.length>0">
         <i class='h-icon-angle-down'></i>
       </span>
   </div>
@@ -21,7 +21,8 @@ export default {
     inlineCollapsed: {
       type: Boolean,
       default: false
-    }
+    },
+    mode: String
   },
   data() {
     return {
