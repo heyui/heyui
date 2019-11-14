4af9762693e50b176db582438b45e44ba0ee0dad
liaohainan <liaohainan@liaohainandeMacBook-Pro.local>
2019-11-14 20:57:22 +0800

    增加中线滑动

--- doc/components/run/components.vue
+++ doc/components/run/components.vue
@@ -10,7 +10,7 @@
     left: 0;
   }
   .code-top-menu {
-    border-bottom: 1px solid #EEE;
+    border-bottom: 1px solid #eee;
     padding: 10px 0;
   }
 
@@ -18,6 +18,38 @@
     display: flex;
     flex: 1;
     overflow: hidden;
+  }
+
+  .split-trigger {
+    border: 1px solid #dcdee2;
+  }
+  .split-trigger-vertical {
+    width: 6px;
+    // height: 100%;
+    background: #f8f8f9;
+    border-top: none;
+    border-bottom: none;
+    cursor: col-resize;
+    @media (max-width: 600px) {
+      &:not(:last-of-type) {
+        display: none;
+      }
+    }
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .split-trigger-bar-con.vertical {
+    display: flex;
+    flex-direction: column;
+    height: 32px;
+  }
+  .split-trigger-bar {
+    width: 4px;
+    height: 1px;
+    background: rgba(23, 35, 61, 0.25);
+    float: left;
+    margin-top: 3px;
   }
 }
 </style>
@@ -30,18 +62,26 @@
         <Button @click="reset">重置Reset</Button>
       </div>
       <div class="code-frame">
-        <Code ref="codePage"></Code>
+        <Code ref="codePage" :style="leftStyle"></Code>
+        <div @mousedown="splitTriggerDown" class="split-trigger split-trigger-vertical">
+          <div class="split-trigger-bar-con vertical">
+            <i class="split-trigger-bar"></i>
+            <i class="split-trigger-bar"></i>
+            <i class="split-trigger-bar"></i>
+            <i class="split-trigger-bar"></i>
+            <i class="split-trigger-bar"></i>
+            <i class="split-trigger-bar"></i>
+          </div>
+        </div>
         <Preview ref="previewPage" :code="code"></Preview>
       </div>
     </div>
   </div>
 </template>
 <script>
-
 import Preview from './components/preview';
 import './js/codemirror.js';
 import sample from './js/example';
-
 export default {
   components: {
     Preview,
@@ -49,7 +89,9 @@ export default {
   },
   data() {
     return {
-      code: null
+      code: null,
+      is_moving: false,
+      leftStyle: {}
     };
   },
   mounted() {
@@ -85,10 +127,35 @@ export default {
         e.preventDefault();
         this.run();
       }
+    },
+    splitTriggerDown(e) {
+      this.is_moving = true;
+      document.addEventListener('mousemove', this.mouseMoveHandler);
+      this.mouseDownHandler();
+      e.preventDefault();
+    },
+    mouseDownHandler() {
+      document.addEventListener('mouseup', this.mouseUpHandler);
+    },
+    mouseUpHandler() {
+      document.removeEventListener('mousemove', this.mouseMoveHandler);
+      document.removeEventListener('mouseup', this.mouseUpHandler);
+    },
+    mouseMoveHandler(e) {
+      if (this.is_moving) {
+        const event = e || window.event;
+        let leftItemWidth = event.x - 20; // code区域左侧有一定宽度要减掉
+        if (leftItemWidth < 100) {
+          leftItemWidth = 100;
+        } else if (leftItemWidth > document.body.clientWidth - 100) {
+          leftItemWidth = document.body.clientWidth - 100;
+        }
+        this.leftStyle = {
+          width: leftItemWidth + 'px'
+        };
+      }
     }
   },
-  computed: {
-
-  }
+  computed: {}
 };
 </script>
