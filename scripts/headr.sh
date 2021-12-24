4526ac56130785012a55d06908b88fe6d2139312
vvpvvp <alias8516@sina.com.cn>
2021-12-24 15:12:15 +0800

    fix: update headr.sh

--- scripts/headr.sh
+++ scripts/headr.sh
@@ -1,4 +1,4 @@
-headr ../dist/heyui.js -o=../dist/heyui.js --version --homepage --copyright --license
-headr .,/dist/heyui.esm.js -o=../dist/heyui.esm.js --version --homepage --copyright --license
+headr ./dist/heyui.js -o=./dist/heyui.js --version --homepage --copyright --license
+headr ./dist/heyui.esm.js -o=./dist/heyui.esm.js --version --homepage --copyright --license
 
 git add themes
