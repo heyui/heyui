71dc943dcc8337a4cec639aa756729974ce44f78
夏悸 <wmails@126.com>
2023-05-16 00:03:23 +0800

    refactor(input): input支持数字类型

--- src/components/input/input.vue
+++ src/components/input/input.vue
@@ -1,11 +1,11 @@
 <template>
-  <input class="h-input" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" />
+  <input class="h-input" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"/>
 </template>
 <script>
 export default {
   name: 'HInput',
   props: {
-    modelValue: String
+    modelValue: [String, Number]
   }
 };
 </script>
